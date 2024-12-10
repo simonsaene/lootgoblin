b19e15427708cdd87c92456bf1236f8712301ab3
Soubanty Simon Saenephommachanh <000089356@mohawkcollege.ca>
2024-12-09 23:21:30 -0500

    like grind visual bug fix

--- resources/views/layouts/likes/like-grind.blade.php
+++ resources/views/layouts/likes/like-grind.blade.php
@@ -1,5 +1,8 @@
 {{-- Add the Like button here --}}
 <div class="d-flex justify-content-center mb-2">
+    @php
+        $likesCount = $spot->likes()->count() - 1;
+    @endphp
     @if($spot->likes()->where('liker_id', auth()->id())->exists())
         <!-- Unlike Form -->
         <form action="{{ route('unlike.grind', $spot->id) }}" method="POST">
@@ -8,7 +11,7 @@
             <input type="hidden" name="user_id" value="{{ $user->id }}">
             <input type="hidden" name="grind_spot_id" value="{{ $spot->id }}">
             <button type="submit" class="btn btn-outline-secondary btn-sm">
-                <i class="bi bi-hand-thumbs-up"></i> {{ $spot->likes()->count() }}
+                <i class="bi bi-hand-thumbs-up"></i> {{ $likesCount }}
             </button>
         </form>
     @else
@@ -18,7 +21,7 @@
             <input type="hidden" name="user_id" value="{{ $user->id  }}">
             <input type="hidden" name="grind_spot_id" value="{{ $spot->id }}">
             <button type="submit" class="btn btn-outline-success btn-sm">
-                <i class="bi bi-hand-thumbs-up"></i> {{ $spot->likes()->count() }}
+                <i class="bi bi-hand-thumbs-up"></i> {{ $likesCount }}
             </button>
         </form>
     @endif
