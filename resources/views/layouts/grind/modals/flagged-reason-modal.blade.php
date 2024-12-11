e5fdc852f88603ba39151b3a6933a5abd5c31a3f
Soubanty Simon Saenephommachanh <000089356@mohawkcollege.ca>
2024-12-11 16:26:32 -0500

    player search session bug fix

--- resources/views/layouts/grind/modals/flagged-reason-modal.blade.php
+++ resources/views/layouts/grind/modals/flagged-reason-modal.blade.php
@@ -1,12 +1,12 @@
-<div class="modal fade" id="flagSessionReasonModal{{ $session->id }}" tabindex="-1" aria-labelledby="flagSessionReasonModalLabel{{ $session->id }}" aria-hidden="true">
+<div class="modal fade" id="flagSessionReasonModal{{ $session_id }}" tabindex="-1" aria-labelledby="flagSessionReasonModalLabel{{ $session_id }}" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="flagSessionReasonModalLabel{{ $session->id }}">Flag Reason for Session:</h5>
+                <h5 class="modal-title" id="flagSessionReasonModalLabel{{ $session_id }}">Flag Reason for Session:</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p>{{ $flaggedSessions[$session->id] }}</p>
+                <p>{{ $flaggedSessions[$session_id] }}</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
