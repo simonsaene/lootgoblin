d8fee76a7cd5dd38a93a59395310d22295b6db26
Soubanty Simon Saenephommachanh <000089356@mohawkcollege.ca>
2024-12-11 16:42:36 -0500

    users can view admin session

--- resources/views/layouts/grind/spot/show-post-flags.blade.php
+++ resources/views/layouts/grind/spot/show-post-flags.blade.php
@@ -12,7 +12,6 @@
     @endif
 @else
     @if(in_array($post_id, array_keys($flaggedPosts)))
-        @include('layouts.grind.modals.flagged-reason-modal')
         <button class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#flagReasonModal{{ $post_id  }}" data-session-id="{{ $post_id  }}">
             <i class="bi bi-flag"></i>
         </button>
